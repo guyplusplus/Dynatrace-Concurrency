9bbb38eb809c7fbd71b532c532b60ad9ab3e7dd9
guyplusplus <18182742+guyplusplus@users.noreply.github.com>
2025-05-17 15:33:29 +0700

    fix test script file locations

--- src/test/java/com/geckotechnology/dynatraceConcurrency/TestConcurrency.java
+++ src/test/java/com/geckotechnology/dynatraceConcurrency/TestConcurrency.java
@@ -6,31 +6,31 @@ public class TestConcurrency {
 	
 	@Test
 	void testLocal1() {
-		String args[] = {"-events", "-timeslot", "5", "-loadDQLResultsFileName", "C:\\GuyData\\DTConcurrencyWorkspace\\dynatraceConcurrency\\src\\test\\resources\\sampleoutput.json"};
+		String args[] = {"-events", "-timeslot", "5", "-loadDQLResultsFileName", ".\\src\\test\\resources\\sampleoutput.json"};
 		Concurrency.main(args);
 	}
 
 	@Test
 	void testLocal2() {
-		String args[] = {"-events", "-loadDQLResultsFileName", "C:\\GuyData\\DTConcurrencyWorkspace\\dynatraceConcurrency\\src\\test\\resources\\sampleoutput.json"};
+		String args[] = {"-events", "-loadDQLResultsFileName", ".\\src\\test\\resources\\sampleoutput.json"};
 		Concurrency.main(args);
 	}
 
 	@Test
 	void testLocal3() {
-		String args[] = {"-events", "-timeslot", "5", "-field.startTime", "messages.processstarttime", "-loadDQLResultsFileName", "C:\\GuyData\\DTConcurrencyWorkspace\\dynatraceConcurrency\\src\\test\\resources\\sampleoutput.json", "-saveDQLResultsFileName", "c:\\tmp\\dqltest3.out"};
+		String args[] = {"-events", "-timeslot", "5", "-field.startTime", "messages.processstarttime", "-loadDQLResultsFileName", ".\\src\\test\\resources\\sampleoutput.json", "-saveDQLResultsFileName", "\\tmp\\dqltest3.out"};
 		Concurrency.main(args);
 	}
 
 	@Test
 	void testLocal4() {
-		String args[] = {"-events", "-timeslot", "5", "-loadDQLResultsFileName", "C:\\GuyData\\DTConcurrencyWorkspace\\dynatraceConcurrency\\src\\test\\resources\\sampleoutputlimited.json"};
+		String args[] = {"-events", "-timeslot", "5", "-loadDQLResultsFileName", ".\\src\\test\\resources\\sampleoutputlimited.json"};
 		Concurrency.main(args);
 	}
 
 	@Test
 	void testRemote1() {
-		String args[] = {"-timeslot", "5", "-saveDQLResultsFileName", "c:\\tmp\\dqltest4.out"};
+		String args[] = {"-timeslot", "5", "-saveDQLResultsFileName", "\\tmp\\dqltest4.out"};
 		Concurrency.main(args);
 	}
 
